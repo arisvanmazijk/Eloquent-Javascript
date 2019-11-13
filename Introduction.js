8be1fcdd9ed36de3c7b0865eb4d1b248c171420c
arisvanmazijk <arisvanmazijk@hotmail.com>
2019-11-13 15:25:16 +0100

    Uncommented

--- Introduction.js
+++ Introduction.js
@@ -1,9 +1,9 @@
-// let total = 0, count = 1;
-// while (count <= 10) {
-//   total += count;
-//   count += 1;
-// }
-// console.log(total);
+let total = 0, count = 1;
+while (count <= 10) {
+  total += count;
+  count += 1;
+}
+console.log(total);
 
 function myFirstFunction(n) {
   if (n == 0) {
