23239663fcbe899d5e37840f6332b072e2c1e82c
arisvanmazijk <arisvanmazijk@hotmail.com>
2019-12-03 21:46:05 +0100

    Chapter 5

---
+++ Chapter_5.js
@@ -0,0 +1,16 @@
+// let array = ["Aris", "Indar", "Janka", "Theo", "Aad"];
+// let names = ["Erszebet", "Jannie", "Agnes", "Niko", "Jeroen", "Indar"];
+
+// function findName(array, names) {
+//     for (let name of names) {
+//         if (array.some(() => {
+//             return array.includes(name);
+//         })) return name;
+//     }   
+// }
+
+// console.log(findName(array, names));
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+console.log(arrays.reduce((a, b) => a.concat(b)));
+// → [1, 2, 3, 4, 5, 6]
