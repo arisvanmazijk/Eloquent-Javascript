7179938be8a4246dc967f2b3e27bb497750cef44
arisvanmazijk <arisvanmazijk@hotmail.com>
2019-12-06 14:19:38 +0100

    Chapter 6

--- Chapter_6.js
+++ Chapter_6.js
@@ -32,19 +32,47 @@ class Group {
         return this.group.includes(val);
     }
     static from(val) {
-        let group = new Group();
+        let group = new Group;
         for (let i of val) {
             group.add(i);
         }
         return group;
     }
+    [Symbol.iterator]() {
+        return new GroupIterator(this);
+      }
 }
-let group = Group.from([10, 20]);
-// console.log(group.has(10));
+
+class GroupIterator {
+    constructor(group) {
+        this.group = group;
+        this.pos = 0;
+    } 
+    next() {
+        if (this.pos >= this.group.group.length) {
+            return {done: true};
+        } else {
+            let result = {value: this.group.group[this.pos], done: false};
+            this.pos++
+            return result;
+        }
+    }
+}
+// let group = Group.from([10, 20]);
+// console.log(group.group);
 // → true
 // console.log(group.has(30));
 // → false
-group.add(10);
-group.delete(10);
+// group.add(10);
+// group.delete(10);
 // console.log(group.has(10));
 // → false
+// for (let value of Group.from(["a", "b", "c"])) {
+//     console.log(value);
+//   }
+
+let map = {one: true, two: true, hasOwnProperty: true};
+
+
+console.log(Object.hasOwnProperty.call(map, "one"));
+// → true
